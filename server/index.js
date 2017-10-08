cf8bb113dc4e8cefd1e1a9968f8d8647792c685a
codonculous <feidameng01@gmail.com>
2017-10-07 21:14:18 -0400

    place more comment

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+//--------------------routing-------------------
 app.get('/admin',(res,rep) => {
   rep.sendFile(publicPath + 'views/admin.html');
 });
@@ -23,6 +24,8 @@ app.get('/',(res,rep) => {
   rep.sendFile(publicPath + 'views/index.html');
 });
 
+
+//---------------------setting up keys to use twitter api-------------------------------
 var T = new Twit({
   consumer_key:         keys.TWITTER_CONSUMER_KEY,
   consumer_secret:      keys.TWITTER_CONSUMER_SECRET,
@@ -31,8 +34,10 @@ var T = new Twit({
   timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
 });
 
-let filter = { track: [] };
+//------------------init the filter for fetching twit ----------------------------
+let filter = { track: [''] };
 
+//---------------io listens on new socket connected on the client side ---------------------
 io.on('connection',(socket) => {
 
   console.log(`New user connected, ${Date.now()}`);
@@ -41,43 +46,74 @@ io.on('connection',(socket) => {
   let streamOn;
   let currentAdminFilter;
 
+//function to fetch twit using stream and post twit to the front using socket.io
+// it will be called when user submits a new hashtag search
+// or admin sets a new filter
   const fetchAndPostTwit = () => {
+
+    //if streaming is currently on, stop it,
+    //since the filter for fetching twit is changed
+    //when user enter an new hashtag or
+    //admin updated the filter
+
     if (streamOn) {
       stream.stop();
     }
 
+    //start a streaming with a filter
     stream = T.stream('statuses/filter', filter);
 
+    //listen on new tweet
     stream.on('tweet', (tweet) => {
+
       console.log('tweet:!!!!!!!!!!!!!!',tweet);
+        // emit new tweet to front. Socket in index.js in the client/views folder is listening to this
         socket.emit('newTwt',tweet);
     });
 
+    //set it to on so the the code at line 57 knows,
+    //I tried if (stream) or (!!stream), didnt seem to work
     streamOn = true;
 
+    //stop the stream when the socket is disconnected, e.g. browser closed
+    // otherwise the streaming will continue as long as the server is on
     socket.on('disconnect',() => {
       stream.stop();
     });
 
   }
 
+//---------You can ignore this part for now, I am still working on it---------------
+// this part is for when admin sets a filter for the twit user would get, by e.g. location, username
   socket.on('adminFilterInput', (adminFilter) => {
+
     currentAdminFilter = adminFilter;
+
     console.log('Filter: ',filter);
+
+    //-----update the filter, not finishd------------------------------
     for(let key in adminFilter) {
       filter[key] ? filter[key].push(adminFilter[key]) : filter[key] = [adminFilter[key]];
     }
     console.log('Filter: ',filter);
 
+    //-----once the filter is updated, call the function below to start new streaming again based on new filter
     fetchAndPostTwit();
   });
+//-----------------------------------------------------------------
 
+//----it listens the event that user submits the hashtag,
+//-----the event is send by the code socket.emit('searchHashtag', hashtagInputBox.val()); at client/js/index  ------------------------
   socket.on('searchHashtag', (hashtag) => {
 
+    //Sanitize the user input
     hashtag = hashtag.trim();
     hashtag = hashtag.charAt(0) === '#' ? hashtag : `#${hashtag}`;
 
-    filter.track.push(hashtag);
+    var currentHashtag = hashtag;
+
+    //update the filter
+    filter.track[0] = hashtag;
     console.log('Filter: ',filter);
 
     fetchAndPostTwit();
